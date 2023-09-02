c91302e64d7ea13256489c9ace91c426cbf63e78
Kumait <137179507+29Kumait@users.noreply.github.com>
2023-09-02 21:32:21 +0200

    Add files via upload

---
+++ 1-find-mentors.js
@@ -0,0 +1,50 @@
+import { modules, students, mentors, classes } from "./hyf.js";
+
+/**
+ * Tjebbe would like help to get a list of possible mentors for a module.
+ * Fill in this function that finds all the mentors that can teach the given module.
+ *
+ * It should return an array of names. So something like:
+ *  ['John', 'Mary']
+ */
+const possibleMentorsForModule = (moduleName) => {
+  // return mentors rather than line 12 and 15
+  const mentorCanTeach = mentors
+    .filter((mentor) => mentor.canTeach.includes(moduleName))
+    .map((mentor) => mentor.name);
+  return mentorCanTeach;
+};
+
+console.log(possibleMentorsForModule("using-apis"));
+
+/**
+ * Tjebbe wants to make it even easier for himself.
+ * Fill in this function that chooses a random mentor to teach the given module.
+ *
+ * It should return a single name.
+ */
+const findMentorForModule = (moduleName) => {
+  const mentorRandom = Math.floor(Math.random() * mentors.length); // error no change: try `-1` to equal the index `(mentors.length - 1)`
+  return possibleMentorsForModule(moduleName)[mentorRandom];
+};
+
+console.log(findMentorForModule("javascript"));
+
+/* 
+
+ * * Terminal ERROR:
+ * some round I got und
+
+ kumait@Kumaits-MBP prep-exercises % node 1-hyf-program/1-find-mentors.js  
+ [ 'Stas', 'Andrej', 'Yash', 'Collin' ]
+ Rohan
+ kumait@Kumaits-MBP prep-exercises % node 1-hyf-program/1-find-mentors.js  
+ [ 'Stas', 'Andrej', 'Yash', 'Collin' ]
+ undefined
+ kumait@Kumaits-MBP prep-exercises % node 1-hyf-program/1-find-mentors.js  
+ [ 'Stas', 'Andrej', 'Yash', 'Collin' ]
+ Yash
+ kumait@Kumaits-MBP prep-exercises % node 1-hyf-program/1-find-mentors.js  
+ [ 'Stas', 'Andrej', 'Yash', 'Collin' ]
+ Stas
+ */
---
+++ 2-class-list.js
@@ -0,0 +1,60 @@
+import { modules, students, mentors, classes } from "./hyf.js";
+
+/**
+ * We would like to have a list of everyone that is currently participating in a class.
+ * This means the students, but also the mentors that are currently teaching the class.
+ * The students should be self explanatory, but to find the mentors you will need to follow these steps:
+ * - Check what the `currentModule` of the class is
+ * - Find the mentor(s) that are `nowTeaching` that module
+ *
+ * Should return the list of names and their roles. So something like:
+ *
+ *  [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }]
+ */
+
+const getPeopleOfClass = (className) => {
+  const currentCla = classes
+    .filter((currentClass) => currentClass.active)
+    .find((currentClass) => currentClass.name === className);
+
+  const mentoring = mentors
+    .filter((mentor) => mentor.nowTeaching === currentCla.currentModule) // the big problem was using find() terminal report `not function`
+    .map((mentor) => ({
+      name: mentor.name,
+      role: "mentor",
+    }));
+
+  const currentStudents = students
+    .filter((student) => student.class === className)
+    .map((student) => ({
+      name: student.name,
+      role: "student",
+    }));
+
+  return [...currentStudents, ...mentoring];
+};
+console.log(getPeopleOfClass("class34"));
+
+/**
+ * We would like to have a complete overview of the current active classes.
+ * First find the active classes, then for each get the people of that class.
+ *
+ * Should return an object with the class names as properties.
+ * Each class name property contains an array identical to the return from `getPeopleFromClass`. So something like:
+ *
+ *  {
+ *    class34: [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }],
+ *    class35: [{ name: 'Jane', role: 'student' }, { name: 'Steve', role: 'mentor' }]
+ *  }
+ */
+const getActiveClasses = () => {
+  const scopeRelease = {};
+  const activeClasses = classes.filter((cla) => cla.active);
+  activeClasses.forEach((cla) => {
+    const peopleOfClass = getPeopleOfClass(cla.name);
+    scopeRelease[cla.name] = peopleOfClass;
+  });
+
+  return scopeRelease;
+};
+console.log(getActiveClasses());
